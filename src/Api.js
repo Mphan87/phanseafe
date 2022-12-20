5938ca7cd1996c806e7d19480354a00963a84dd7
Michael <xmykyx@gmail.com>
2022-12-20 10:03:33 -0800

    update

--- src/Api.js
+++ src/Api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-// const BASE_API_URL = "http://localhost:3001" || "https://phanseadb.herokuapp.com";
+const BASE_API_URL = "https://phanseadb.herokuapp.com";
 
-const BASE_API_URL = process.env.BASE_API_URL || "http://localhost:3001";
+// const BASE_API_URL = process.env.BASE_API_URL || "http://localhost:3001";
 
 
 /* 
